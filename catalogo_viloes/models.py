73d14c11b4787d1c72fd211567c0f279b5282bb5
Ana Beatriz Morita <anabemorita@gmail.com>
2019-07-02 15:59:24 -0300

    criar model vilao

--- catalogo_viloes/models.py
+++ catalogo_viloes/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class Vilao(models.Model):
+
+    armas_opcoes = [
+        ('machado', 'Machado'),
+        ('escada', 'Escada'),
+        ('espada', 'Espada'),
+        ('tesoura', 'Tesoura'),
+        ('veneno', 'Veneno'),
+        ('pexera', 'Pexera'),
+        ('serra elétrica', 'Serra Elétrica'),
+        ('punhos', 'Punhos'),
+        ('ak47', 'AK47'),
+        ('almofada', 'Almofada'),
+        ('outros', 'Outros'),
+        ('desarmado', 'Desarmado')
+    ]
+
+    nivel_valor = [
+        (1,1),
+        (2,2),
+        (3,3),
+        (4,4),
+        (5,5),
+    ]
+
+    nome = models.CharField(max_length=30)
+    categoria = models.CharField(max_length=10)
+    habilidades = models.TextField()
+    mortes = models.PositiveIntegerField()
+    armas = models.CharField(max_length=20, choices=armas_opcoes)
+    nivel_maldade = models.PositiveIntegerField(choices=nivel_valor)
+    vivo = models.BooleanField(default=True)
+    foto = models.ImageField(upload_to='')
