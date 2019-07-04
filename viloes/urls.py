eb7abab4a8ccfb4b0006d4af2dc31a7f453181c5
Ana Beatriz Morita <anabemorita@gmail.com>
2019-07-04 12:57:47 -0300

    corrigir rotas para media estatica

--- viloes/urls.py
+++ viloes/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from catalogo_viloes import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.mostrar_index),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
