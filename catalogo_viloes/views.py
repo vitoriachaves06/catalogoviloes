03a08911d444e6d3ea2599db63983a45d6a145c4
Ana Beatriz Morita <anabemorita@gmail.com>
2019-07-04 12:58:36 -0300

    retornar todos viloes cadstrados

--- catalogo_viloes/views.py
+++ catalogo_viloes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from catalogo_viloes.models import Vilao
 # Create your views here.
 
 def mostrar_index(request):
-    return render(request, 'index.html')
+    viloes = Vilao.objects.all()
+    return render(request, 'index.html', {'viloes': viloes})
